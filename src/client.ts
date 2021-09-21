f1514e5fc7fe5d319f566f34a8b12dd3029fab3d
CountRochester <count_rochester@mail.ru>
2021-09-22 00:59:00 +0300

    Adds some error handling

--- src/client.ts
+++ src/client.ts
@@ -3,7 +3,7 @@ import nodeFetch, {
   Response as NodeResponse
 } from 'node-fetch'
 import { formBody, getFetchOptions, getNodeFetchOptions } from './lib'
-import { ERROR_GQL_REQUEST } from './errors'
+import { ERROR_GQL_REQUEST, ERRORS_RECEIVED, GQLError } from './errors'
 
 export interface GraphQLClientOptions {
   graphqlEndpoint: string
@@ -14,11 +14,20 @@ export type Variables = {
   [key: string]: number|string|boolean|File
 }
 
+export type GQLResult = {
+  data?: any
+  errors?: any[]
+}
+
 export class GraphQLClient {
   #token?: string
 
   #graphqlEndpoint: string
 
+  /**
+   * @param options.graphqlEndpoint - graphQl endpoint
+   * @param options.token - bearer token for auth
+   */
   constructor ({ graphqlEndpoint, token }: GraphQLClientOptions) {
     this.#graphqlEndpoint = graphqlEndpoint
     if (token) {
@@ -40,7 +49,6 @@ export class GraphQLClient {
   }
 
   private async fetchRequest (body: BodyInit|NodeBodyInit): Promise<any> {
-    // let fetch
     let response!: Response|NodeResponse
     if (global.fetch !== undefined) {
       const options = getFetchOptions({
@@ -53,25 +61,35 @@ export class GraphQLClient {
       } as { body: NodeBodyInit, token: string })
       response = await nodeFetch(this.#graphqlEndpoint, options)
     }
-    try {
-      if (!response.ok) {
-        throw ERROR_GQL_REQUEST(response.statusText)
-      }
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const result = await response.json()
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return result
-    } catch (err) {
-      console.log(err)
-      return undefined
+    if (!response.ok) {
+      throw ERROR_GQL_REQUEST(response.statusText)
     }
+    const result = await response.json() as GQLResult
+    if (!result.data) {
+      throw ERROR_GQL_REQUEST('No data received')
+    }
+    if (result.errors) {
+      throw ERRORS_RECEIVED(result.errors)
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return result.data
   }
 
   async request (query: string, variables: Variables): Promise<any> {
-    const body = formBody(query, variables)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const result = await this.fetchRequest(body)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return result
+    try {
+      const body = formBody(query, variables)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const result = await this.fetchRequest(body)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return result
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new GQLError(err.message, {
+          query, variables
+        }, err)
+      }
+      console.error(err)
+      return undefined
+    }
   }
 }
--- src/errors.ts
+++ src/errors.ts
@@ -2,3 +2,18 @@ export const ERROR_GQL_REQUEST = (msg: string): Error => new Error(`Error graphQ
 export const INVALID_GQL_REQUEST = (): Error => new Error('Error graphQl request')
 // eslint-disable-next-line max-len
 export const ERROR_GQL_NO_VARIABLE = (name: string): Error => new Error(`No ${name} proper variable found in variables.`)
+
+export class GQLError extends Error {
+  detail?: any
+
+  originError?: Error|GQLError
+
+  constructor (message: string, detail?: any, originError?: Error|GQLError) {
+    super(message)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    this.detail = detail
+    this.originError = originError
+  }
+}
+
+export const ERRORS_RECEIVED = (errors: any[]): GQLError => new GQLError('Received errors.', errors)
